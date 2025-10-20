210b92c617e182b49ee9da430a87020bc7ffad77
NikolayTola <kolyatola@mail.ru>
2025-10-20 16:35:34 +0700

    fix pop-up create and crush server

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   hostUrl: 'http://45.135.182.251:3000',
-  rpcUrl: 'https://sepolia.infura.io/v3/6113828fd8e8448f9a9e3fa7962e2cc6',
+  rpcUrl: 'https://sepolia.infura.io/v3/8205c522bcda4efb89497a065e8a6e04',
   tokenAddress: '0x7b44196a1D950FD80506d3782Cf2ad563C76C136',
 };
